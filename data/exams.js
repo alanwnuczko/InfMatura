35b4d314357546f7aae3a4bbcb14aae07350a0ad
Alan <alanwnuczko@protonmail.com>
2026-08-31 15:22:58 +0200

    Dodanie Maj 2015 PR F2015 do strony oraz wygenerowanie podstrony

--- data/exams.js
+++ data/exams.js
@@ -228,6 +228,20 @@ var EXAMS_LIST = [
     "hasData": true,
     "hasSolution": true,
     "hasZasady": true
+  },
+  {
+    "id": "2015-05_PR_F2015",
+    "year": 2015,
+    "month": "05",
+    "type": "PR",
+    "formula": "F2015",
+    "parts": [
+      "Arkusz_cz1",
+      "Arkusz_cz2"
+    ],
+    "hasData": true,
+    "hasSolution": true,
+    "hasZasady": true
   }
 ];
 
